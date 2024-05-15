1ae08163b05fbbcf92229c5f920e5ec8cd2854fc
Stephen Wang <stephen.wang.24@dartmouth.edu>
2024-05-14 21:55:21 -0400

    profile model edit

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -1,13 +1,14 @@
 import mongoose, { Schema } from 'mongoose';
+import Post from './Post';
 
 const ProfileSchema = new Schema({
   name: String,
   email: String,
   password: String,
   user_type: String,
-  followers: String,
-  following: String,
-  highlights: [],
+  followers: [String],
+  following: [String],
+  highlights: [Post],
 }, {
   toObject: { virtuals: true },
   toJSON: { virtuals: true },
